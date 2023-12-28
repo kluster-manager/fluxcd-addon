1a98b4c57e363b701777de0265ac717fdfef5c86
Tamal Saha <tamal@appscode.com>
2023-12-27 16:59:14 -0800

    Restructure code (#5)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -1,9 +1,27 @@
+/*
+Copyright AppsCode Inc. and Contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package manager
 
 import (
 	"context"
 	"embed"
-	fluxapi1alpha1 "github.com/kluster-manager/fluxcd-addon/api/api/v1alpha1"
+
+	fluxapi1alpha1 "github.com/kluster-manager/fluxcd-addon/apis/fluxcd/v1alpha1"
+
 	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -35,10 +53,8 @@ const (
 	AgentInstallNamespace = "flux-system"
 )
 
-var (
-	// scheme is a runtime.Scheme for managing API resources.
-	scheme = runtime.NewScheme()
-)
+// scheme is a runtime.Scheme for managing API resources.
+var scheme = runtime.NewScheme()
 
 func init() {
 	utilruntime.Must(fluxapi1alpha1.AddToScheme(scheme))
