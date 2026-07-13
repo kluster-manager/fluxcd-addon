0d2fb70ab14bf82f3bf02db3daf296d5a52bb3db
Tamal Saha <tamal@appscode.com>
2026-07-14 02:45:03 +0600

    Modernize golangci-lint config
    - Add a .golangci.yml (v2) in place of inline golangci-lint flags in the
      Makefile lint target
    - Enable bodyclose and prealloc linters alongside unparam
    - Use linters.exclusions.paths for generated/client/vendor
    - Use gofumpt + goimports formatters (gofumpt is a stricter gofmt superset)
    - Apply resulting gofumpt formatting and simplify an embedded-field selector
      flagged by staticcheck (QF1008)
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/fluxcd/v1alpha1/types_test.go
+++ apis/fluxcd/v1alpha1/types_test.go
@@ -26,7 +26,8 @@ import (
 )
 
 func TestDefaultValues(t *testing.T) {
-	checker := sc.New(os.DirFS("../../.."),
+	checker := sc.New(
+		os.DirFS("../../.."),
 		sc.TestCase{Obj: v1alpha1.FluxCDConfigSpec{}, File: "pkg/manager/agent-manifests/flux2/values.yaml"},
 	)
 	checker.TestAll(t)
